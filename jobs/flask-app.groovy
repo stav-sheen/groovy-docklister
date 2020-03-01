22324bede8d2e2cbcdb9052b4bba8637cb071973
root <root@ip-172-31-30-109.eu-west-3.compute.internal>
2020-03-01 10:16:32 +0000

    added first job

---
+++ jobs/flask-app.groovy
@@ -0,0 +1,25 @@
+job('DockerBuildandPublishDSL') {
+    description('Build and Publish flask app image to dockerhub')
+     scm {
+        git {
+        remote {
+                url('https://github.com/stav-sheen/groovy-docklister.git')
+	        credentials('${Git-Creds}')
+            }
+            wrappers {
+                preBuildCleanup()
+            } 
+            branch('*/' + 'master')
+        }
+    }
+
+    steps {
+        dockerBuildAndPublish {
+            repositoryName('stavsheen/flasker')
+            tag('${env.BUILD_ID}')
+	    tag('latest')
+            registryCredentials('')
+            dockerfileDirectory('./flask-app')
+        }
+    }
+}
